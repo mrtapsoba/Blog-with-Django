52350ab3f6f1321809c3384df0209339f33ff037
TAPSOBA Abdoul Kader <ktapsoba80@gmail.com>
2024-10-22 22:13:03 +0000

    templates et Statics

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path("<int:id>/", views.post, name='post')
 ]
